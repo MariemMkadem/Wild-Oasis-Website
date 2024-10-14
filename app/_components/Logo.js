a32764cc7231e73b91f6bc75b10a1885c2b32fca
MariemMkadem <mmkadem11@gmail.com>
2024-10-14 14:59:08 +0200

    setting up project

---
+++ app/_components/Logo.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import Image from "next/image";
+import logo from "@/public/Logo.png";
+
+export default function Logo() {
+  return (
+    <Link href="/" className="flex items-center gap-4 z-10">
+      <Image
+        src={logo}
+        height="60"
+        width="60"
+        alt="The Wild Oasis logo"
+        quality={100}
+      />
+      <span className="text-xl font-semibold text-primary-100">
+        The Wild Oasis
+      </span>
+    </Link>
+  );
+}
